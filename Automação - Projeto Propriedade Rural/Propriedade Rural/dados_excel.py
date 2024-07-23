ec80bc01ffdaeca6e50b005f33558d024c0817be
BEATRIZ158 <beatrizmesquita158@gmail.com>
2024-07-23 09:24:23 -0300

    Alteração

--- Automação - Projeto Propriedade Rural/Propriedade Rural/dados_excel.py
+++ Automação - Projeto Propriedade Rural/Propriedade Rural/dados_excel.py
@@ -2,7 +2,7 @@ import openpyxl
 
 def salvar_em_excel(animal):
     try:
-        wb = openpyxl.load_workbook('Automação - Projeto Propriedade Rural/Propriedade Rural/dados_animais.xlsx')
+        wb = openpyxl.load_workbook('dados_animais.xlsx')
         ws = wb.active
         
         # Verificar se as colunas já foram adicionadas
@@ -16,4 +16,4 @@ def salvar_em_excel(animal):
     # Adicionar os dados do animal
     ws.append([animal.numero_brinco, animal.peso, animal.data_entrada, animal.numero_piquet, animal.preco_racao, animal.preco_silo, animal.racao, animal.silo])
     
-    wb.save('Automação - Projeto Propriedade Rural/Propriedade Rural/dados_animais.xlsx')
+    wb.save('dados_animais.xlsx')
